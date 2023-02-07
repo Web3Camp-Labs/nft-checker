3ee1a6f1e88ec45ec28d57478d8299f26d5cf191
sr <7518647+wendychaung@users.noreply.github.com>
2023-02-07 14:16:16 +0800

    Update headTop.tsx

--- src/components/headTop.tsx
+++ src/components/headTop.tsx
@@ -1,16 +1,8 @@
-import React, {useEffect, useState} from "react";
+import React from "react";
 import { Container, Row, Col } from 'react-bootstrap';
-import ChainJson from "../api/chain.json";
-import {ethers} from "ethers";
-import styled from "styled-components";
 import LogoImg from "../assets/web3camp.png";
 
-
-
 export default function HeaderTop() {
-
-
-
     return <div className="header">
         <Container>
             <Row>
@@ -19,16 +11,6 @@ export default function HeaderTop() {
                         <img src={LogoImg} alt=""/>
                     </a>
                 </Col>
-
-                {/*<Col className="headerTxt" md={8} xs={12}>NFT Checker</Col>*/}
-                {/*<Col className="headetRht" md={4} xs={12}>*/}
-                {/*    {*/}
-                {/*        chainName && <AddressBox>{chainName}</AddressBox>*/}
-                {/*    }*/}
-                {/*    {*/}
-                {/*       ! chainName && <div>Please install MetaMask</div>*/}
-                {/*    }*/}
-                {/*</Col>*/}
             </Row>
         </Container>
     </div>
